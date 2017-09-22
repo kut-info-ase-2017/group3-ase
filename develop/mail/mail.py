959c02fcd233b42321a307967afda51d03853390
ayato1995 <ayato.k070206@outlook.jp>
2017-09-23 00:23:18 +0900

    メールに画像を添付して送信できるようにした

--- develop/mail/mail.py
+++ develop/mail/mail.py
@@ -1,3 +1,10 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+from email.header import Header
+from email.utils import formatdate
+
 import smtplib
 import sys
 
@@ -12,15 +19,43 @@ if result[0] != 250:
 #    print('starttls is failed.')
 #    sys.exit()
 
+# 送信先アドレス，送信アドレスの設定
+send_mail = 'send@gmail.com'
+receive_mail = 'receive@gmail.com'
+
+encoding = 'utf-8'
+
+# 件名の設定
+subject = 'test'
+# 本文の設定
+body = 'This is a test mail.'
+# 添付画像のパスの指定
+image_pass = '../photo/unknown/who0.jpg'
+file_name = 'who0.jpg'
+
+msg = MIMEMultipart()
+mt = MIMEText(body.encode(encoding), "plain", encoding)
+
+img = open(image_pass, 'rb').read()
+mj = MIMEImage(img, 'jpeg', filename=file_name)
+mj.add_header('Content-Disposition', 'attachment', failname=file_name)
+msg.attach(mj)
+msg.attach(mt)
+
+msg['Subject'] = Header(subject, encoding)
+msg['From'] = send_mail
+msg['To'] = receive_mail
+msg['Date'] = formatdate()
+
 password = input()
-result = smtp_obj.login('send@gmail.com', password)
+result = smtp_obj.login(send_mail, password)
 if result[0] != 235:
     print('login is failed.')
     sys.exit()
 
-result = smtp_obj.sendmail('send@gmail.com', 'receive@gmail.com', 'Subject: test\nThis is a test mail.')
+result = smtp_obj.sendmail(send_mail, [receive_mail], msg.as_string())
 if result != {}:
     print('sendmail is failed.')
     sys.exit()
-
+smtp_obj.close()
 print('ok')
